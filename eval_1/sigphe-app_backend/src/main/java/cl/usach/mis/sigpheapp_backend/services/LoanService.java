49cefbebbcce2798898fffc39cbc66ecd33b3da0
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2025-09-19 16:58:01 -0300

    ADD: Servicio para prestamo (LoanService) con metodo getAllLoansSummary.

---
+++ eval_1/sigphe-app_backend/src/main/java/cl/usach/mis/sigpheapp_backend/services/LoanService.java
@@ -0,0 +1,44 @@
+package cl.usach.mis.sigpheapp_backend.services;
+
+import cl.usach.mis.sigpheapp_backend.dtos.LoanFullSummaryDTO;
+import cl.usach.mis.sigpheapp_backend.dtos.LoanSummaryDTO;
+import cl.usach.mis.sigpheapp_backend.entities.LoanEntity;
+import cl.usach.mis.sigpheapp_backend.entities.LoanStatusEntity;
+import cl.usach.mis.sigpheapp_backend.entities.UserEntity;
+import cl.usach.mis.sigpheapp_backend.repositories.LoanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class LoanService {
+
+    @Autowired private LoanRepository loanRepository;
+
+    public List<LoanSummaryDTO> getAllLoansSummary() {
+        return loanRepository.findAll().stream()
+                .map(this::toLoanSummaryDTO)
+                .collect(Collectors.toList());
+    }
+
+    public LoanSummaryDTO toLoanSummaryDTO(LoanEntity loan) {
+        Objects.requireNonNull(loan, "LoanEntity cannot be null");
+        LoanSummaryDTO dto = new LoanSummaryDTO();
+        dto.setId(loan.getId());
+        dto.setStartDate(loan.getStartDate());
+        dto.setReturnDate(loan.getReturnDate());
+        dto.setDueDate(loan.getDueDate());
+        dto.setTotalAmount(loan.getTotalAmount());
+        dto.setLoanStatus(Optional.ofNullable(loan.getLoanStatus())
+                .map(LoanStatusEntity::getName)
+                .orElse("Unknown"));
+        dto.setCustomerName(Optional.ofNullable(loan.getCustomerUser())
+                .map(UserEntity::getName)
+                .orElse("Unknown"));
+        return dto;
+    }
+}
