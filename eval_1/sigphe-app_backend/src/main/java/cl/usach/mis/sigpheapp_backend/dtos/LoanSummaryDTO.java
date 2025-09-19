dee5aedec123e272eecb2bb4a881fcff16b94624
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2025-09-19 16:44:42 -0300

    ADD: Agrega etiqueta de getter, setter, constructores a LoanSummaryDTO

--- eval_1/sigphe-app_backend/src/main/java/cl/usach/mis/sigpheapp_backend/dtos/LoanSummaryDTO.java
+++ eval_1/sigphe-app_backend/src/main/java/cl/usach/mis/sigpheapp_backend/dtos/LoanSummaryDTO.java
@@ -1,8 +1,15 @@
 package cl.usach.mis.sigpheapp_backend.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoanSummaryDTO {
     private Long id;
     private LocalDateTime startDate, returnDate;
