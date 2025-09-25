d0d4a47187653c7993a21e04719f733be57837ce
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2025-09-25 02:23:32 -0300

    ADD: Se configura llamadas con axios

---
+++ eval_1/sigphe-app_frontend/src/http-common.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const sigpheAppBackendServer = import.meta.env.VITE_SIGPHE_APP_BACKEND_SERVER;
+const sigpheAppBackendPort = import.meta.env.VITE_SIGPHE_APP_BACKEND_PORT;
+
+// console.log(payrollBackendServer)
+// console.log(payrollBackendPort)
+
+export default axios.create({
+    baseURL: `http://${sigpheAppBackendServer}:${sigpheAppBackendPort}`,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
