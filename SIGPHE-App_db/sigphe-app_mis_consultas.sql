e89b7d533f87a7b3a169fef416c8e7a66c2b8fff
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2025-10-25 17:16:23 -0300

    ADD: comentarios mejorados

--- SIGPHE-App_db/sigphe-app_mis_consultas.sql
+++ SIGPHE-App_db/sigphe-app_mis_consultas.sql
@@ -18,7 +18,7 @@ select * from loan_details ld;
 select * from penalties p;
 
 -- Consultas optimizadas para test
--- USUARIOS CON SU ESTADO Y TIPO
+-- Users
 select
     u.id as id,
     u.name as user_name,
@@ -29,27 +29,25 @@ from users u
 join user_statuses us on u.user_status_id = us.id
 join user_types ut on u.user_type_id = ut.id;
 
--- Herramientas con su categoria, estado y modelo
+-- Tools
 select
     t.id as id,
     t.name as tool_name,
-    t.rental_value as rep_value,
+    t.replacement_value as rep_value,
     t.rental_value as rent_value,
     m.name as model,
     tc.name as category,
-    ts.name as status,
-    l.id as loan_id
+    ts.name as status
 from tools t
 join tool_categories tc on t.tool_category_id = tc.id
 join tool_statuses ts on t.tool_status_id = ts.id
-join models m on t.model_id = m.id
-join loan_details ld on t.id = ld.tool_id
-join loans l on ld.loan_id = l.id;
+join models m on t.model_id = m.id;
 
--- Kardex con herramienta, tipo de movimiento y trabajador responsable
+-- Kardex
 select
     k.id as id,
     k.date_time as date_register,
+    t.id as tool_id,
     t.name as tool_name,
     k.quantity as quantity,
     kt.name as kardex_type,
@@ -59,7 +57,7 @@ join tools t on k.tool_id = t.id
 join kardex_types kt on k.kardex_type_id = kt.id
 join users u on k.worker_user_id = u.id;
 
--- Préstamos con cliente, estado y detalles
+-- Loans
 select
     l.id as id,
     l.start_date as start_date,
@@ -75,7 +73,7 @@ from loans l
 join users u on l.customer_user_id = u.id
 join loan_statuses ls on l.loan_status_id = ls.id;
 
--- Detalles de préstamos con herramienta y valor de renta en el momento del préstamo
+-- Loan_details
 select
     l.id as loan_id,
     ld.id as detail_id,
@@ -86,7 +84,7 @@ from loans l
 join loan_details ld on l.id = ld.loan_id
 join tools t on ld.tool_id = t.id;
 
--- Penalidades con estado, tipo y usuario asociado
+-- Penalties
 select
     l.id as loan_id,
     p.id as penalty_id,
@@ -100,7 +98,7 @@ join penalty_statuses ps on p.penalty_status_id = ps.id
 join penalty_types pt on p.penalty_type_id = pt.id
 join loans l on p.loan_id = l.id;
 
---
+-- Herramientas más prestadas en general
 select t.id as id, t.name as tool_name, m.name as model, b.name as brand, count(ld.tool_id) as quantity
 from loan_details ld
 join tools t on ld.tool_id = t.id
@@ -108,7 +106,7 @@ join models m on t.model_id = m.id
 join brands b on m.brand_id = b.id 
 group by ld.tool_id, t.id, m.id, b.id 
 order by quantity desc
-limit 3;
+limit 5;
 
 select t.id as id, t.name as tool_name, m.name as model, b.name as brand, count(ld.tool_id) as quantity
 from loan_details ld
@@ -121,14 +119,24 @@ group by ld.tool_id, t.id, m.id, b.id
 order by quantity desc
 limit 3;
 
-select u.name as userName, u.email as userEmail, us.name as userStatus, ut.name as userType, l.id as loanId, ls.name as statusLoan, l.due_date as returnDate
+select u.name as userName, u.email as userEmail, us.name as userStatus, ut.name as userType, l.id as loanId, ls.name as statusLoan, l.due_date as dueDate
 from users u 
 join loans l on u.id = l.customer_user_id
 join loan_statuses ls on ls.id = l.loan_status_id 
 join user_statuses us on us.id = u.user_status_id 
 join user_types ut on ut.id = u.user_type_id 
-where l.due_date between '2025-09-20T23:25:11.271' and '2025-09-24T23:25:11.271' and (ls.name like '%Atrasada' or ls.name like '%Vigente')
-order by l.due_date desc
-limit 3;
+where l.due_date between '2025-09-20T23:25:11.271' and '2025-10-25T23:25:11.271' and (ls.name like '%Atrasada' or ls.name like '%Vigente')
+order by l.due_date desc;
+-- limit 3;
 
+-- contar por cliente la cantidad de prestamos activos (vigentes y atrasados) que esten atrasados
+select u.name as userName, u.email as userEmail, us.name as userStatus, ut.name as userType, count(l.id) as totalOverdueLoans
+from users u
+    join loans l on u.id = l.customer_user_id
+    join loan_statuses ls on ls.id = l.loan_status_id
+    join user_statuses us on us.id = u.user_status_id
+    join user_types ut on ut.id = u.user_type_id
+where l.due_date between '2025-09-20T23:25:11.271' and '2025-10-25T23:25:11.271' and (ls.name like '%Atrasada' or ls.name like '%Vigente')
+group by u.name, u.email, us.name, ut.name
+order by totalOverdueLoans desc;
 
